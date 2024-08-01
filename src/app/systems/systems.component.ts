29b40e30ffbca0e58e4dad9fd2d269888208af53
Santiago Bruno <soysantiagobruno@gmail.com>
2024-08-01 01:06:19 -0300

    creo el componente systems

---
+++ src/app/systems/systems.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-systems',
+  standalone: true,
+  imports: [],
+  templateUrl: './systems.component.html',
+  styleUrl: './systems.component.css'
+})
+export class SystemsComponent {
+
+}

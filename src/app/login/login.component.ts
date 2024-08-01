6d5b88aeb7d7339c205e0685bc15666efd66a76e
Santiago Bruno <soysantiagobruno@gmail.com>
2024-07-31 22:25:14 -0300

    termino los validadores del login y sus estilos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,25 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+
+  formularioLogin = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+  
+  
+  onSubmit() {
+    console.log(this.formularioLogin.value);
+    console.log(this.formularioLogin.status);
+  }
 
 }

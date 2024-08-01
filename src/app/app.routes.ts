293c3d2c8188f5df6aef9a6ae1ef88f24f68595e
Santiago Bruno <soysantiagobruno@gmail.com>
2024-08-01 02:30:36 -0300

    redireccionamiento en caso de acceso a ruta no definida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { SystemsComponent } from './systems/systems.component';
 export const routes: Routes = [
     {path: "login", component: LoginComponent},
     {path: "systems", component: SystemsComponent},
-    
-//    {path: '**', redirectTo: '', pathMatch: 'full'}
+
+    // si ingreso a cualquier ruta que no esté definida, se redirige a /systems o  a /login, dependiendo del estado de autenticación. En este caso es indiferente /login de /systems ya que se redirijiran entre ellos dependiendo el estado de autenticación
+    {path: '**', redirectTo: 'login', pathMatch: 'full'}
 ];
